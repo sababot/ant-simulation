24370ab1533181e3a5c76c9c939aa01f3e4efcc5
sababot <aitor@garciahombrados.com>
2023-01-17 11:26:46 +0100

    trails

--- scripts/script-test.js
+++ scripts/script-test.js
@@ -1,97 +1,21 @@
+// CANVAS CONTEXT
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 
-// INITf
+// KEYBOARD INPUT
 document.addEventListener('keydown', aim, true);
-//document.addEventListener('keyup', aim2, true);
-
 
 // GLOBAL VARIABLES
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-/*
-var left = false;
-var right = false;
-var up = false;
-var down = false;
-let jump = 0;
-let rotation = 0;
-*/
 var pause = false;
 let home_radius = 30;
 
 let secondsPassed = 0;
 let oldTimeStamp = 0;
 
-/* PLAYER ANT VARIABLE
-function PlayerAnt(x, y, infected){
-    this.x = x;
-    this.y = y;
-    this.dx = 0;
-    this.dy = 0;
-    this.speed_rate = 0;
-    this.rotation = 0;
-    this.max_speed = 1.5;
-    this.angular_speed = 2;
 
-    this.draw = function(){
-        var img = new Image();
-        img.src = 'images/player-ant.png';
-
-        c.save(); // save current state
-        c.translate(this.x - 7.5, this.y - 7.5);
-        c.rotate(this.rotation * Math.PI / 180); // rotate
-        c.translate(-this.x - 7.5,-this.y - 7.5);
-        c.drawImage(img, this.x - 7.5, this.y - 7.5, 30, 30); // draws a chain link or dagger
-        c.restore(); // restore original states (no rotation etc)
-
-        ///////////////////////////
-
-        if (right == true && up == true){
-            this.rotation += this.angular_speed;
-        }
-
-        else if (left == true && up == true){
-            this.rotation -= this.angular_speed;
-        }
-
-        ////////////////
-        if (up == false){
-            if (this.speed_rate > 0){
-                this.speed_rate -= 0.5;
-            }
-        }
-        ////////////////
-        else if (up == true){
-            if (this.speed_rate < this.max_speed){
-                this.speed_rate += 0.5;
-            }
-        }
-
-        playerVariable.dx = Math.cos((this.rotation - 90) * (Math.PI / 180)) * this.speed_rate;
-        playerVariable.dy = Math.sin((this.rotation - 90) * (Math.PI / 180)) * this.speed_rate;
-
-        if (this.y > window.innerHeight){
-            this.y = window.innerHeight;
-        }
-        else if (this.y < 15){
-            this.y = 15;
-        }
-
-        if (this.x > window.innerWidth){
-            this.x = window.innerWidth;
-        }
-        else if (this.x < 15){
-            this.x = 15;
-        }
-
-        // Movement
-        this.x += this.dx;
-        this.y += this.dy;
-    }
-}
-*/
 
 // ANT VARIABLE
 function Ant(x, y, rotation, infected){
@@ -99,61 +23,75 @@ function Ant(x, y, rotation, infected){
     this.y = y;
     this.dx = 0;
     this.dy = 0;
+
     this.angular_rotation = rotation;
-    this.speed = 1.5;
     this.rotation = rotation;
-    this.count = 0;
-    this.dir = 0;
     this.angular_speed = 2;
 
+    this.speed = 1.5;
+    this.movement_count = 0;
+    this.dir = 0;
+
+    this.trail_points = [];
+    this.trail_count = 0;
+
+    this.movement = function() {
+        // decide random direction to move
+        if (this.movement_count == 0 && Math.floor((Math.random() * 3) + 1) == 1){
+            this.movement_count = Math.floor((Math.random() * 10) + 1);
+            this.dir = 0;
+        }
+
+        else if (this.movement_count == 0 && Math.floor((Math.random() * 3) + 1) == 2){
+            this.movement_count = Math.floor((Math.random() * 10) + 1);
+            this.dir = 1;
+        }
+
+        else if (this.movement_count == 0 && Math.floor((Math.random() * 3) + 1) == 3){
+            this.movement_count = Math.floor((Math.random() * 50) + 15);
+            this.dir = 2;
+        }
+
+        // perform random direction rotation
+        if (this.movement_count > 0 && this.dir == 1){
+            this.rotation += this.angular_speed;
+            this.movement_count -= 1;
+        }
+
+        else if (this.movement_count > 0 && this.dir == 0){
+            this.rotation -= this.angular_speed;
+            this.movement_count -= 1;
+        }
+
+        else if (this.movement_count > 0 && this.dir == 2){
+            this.rotation += 0;
+            this.movement_count -= 1;
+        }
+
+        // calculate movement with rotation
+        this.dx = Math.cos((this.rotation - 90) * (Math.PI / 180)) * this.speed;
+        this.dy = Math.sin((this.rotation - 90) * (Math.PI / 180)) * this.speed;
+
+        // perform movement
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
     this.draw = function(){
+        // load ant image
         this.img = new Image();
         this.img.src = 'images/ant.png';
 
+        // draw ant
         c.save(); // save current state
         c.translate(this.x - 7.5, this.y - 7.5);
         c.rotate(this.rotation * Math.PI / 180); // rotate
         c.translate(-this.x - 7.5,-this.y - 7.5);
         c.drawImage(this.img, this.x - 7.5, this.y - 7.5, 30, 30); // draws a chain link or dagger
         c.restore(); // restore original states (no rotation etc)
+    }
 
-        ///////////////////////////
-
-        if (this.count == 0 && Math.floor((Math.random() * 3) + 1) == 1){
-            this.count = Math.floor((Math.random() * 10) + 1);
-            this.dir = 0;
-        }
-
-        else if (this.count == 0 && Math.floor((Math.random() * 3) + 1) == 2){
-            this.count = Math.floor((Math.random() * 10) + 1);
-            this.dir = 1;
-        }
-
-        else if (this.count == 0 && Math.floor((Math.random() * 3) + 1) == 3){
-            this.count = Math.floor((Math.random() * 50) + 15);
-            this.dir = 2;
-        }
-
-        if (this.count > 0 && this.dir == 1){
-            this.rotation += this.angular_speed;
-            this.count -= 1;
-        }
-
-        else if (this.count > 0 && this.dir == 0){
-            this.rotation -= this.angular_speed;
-            this.count -= 1;
-        }
-
-        else if (this.count > 0 && this.dir == 2){
-            this.rotation += 0;
-            this.count -= 1;
-        }
-
-        ////////////////
-
-        this.dx = Math.cos((this.rotation - 90) * (Math.PI / 180)) * this.speed;
-        this.dy = Math.sin((this.rotation - 90) * (Math.PI / 180)) * this.speed;
-
+    this.boundaries = function() {
         if (this.y > window.innerHeight){
             if (this.rotation <= 180)
                 this.rotation -= 2;
@@ -180,19 +118,44 @@ function Ant(x, y, rotation, infected){
             else
                 this.rotation += 2;
         }
+    }
 
-        // Movement
-        this.x += this.dx;
-        this.y += this.dy;
+    this.trail = function() {
+        var max_trail_count = 50;
+        var trail_point_lifetime = 1000;
+
+        if (this.trail_count == 0){
+            this.trail_points.push([this.x, this.y, trail_point_lifetime])
+            this.trail_count = max_trail_count;
+        }
+
+        else
+            this.trail_count--;
+
+        for (var i = 0; i < this.trail_points.length; i++){
+            if (this.trail_points[i][2] > 0){
+                c.beginPath();
+                c.arc(this.trail_points[i][0] - 10, this.trail_points[i][1] - 10, this.trail_points[i][2]/200, 0, 2 * Math.PI);
+                c.fillStyle = "darkblue";
+                c.fill();
+                this.trail_points[i][2] -= 1;
+            }
+            else
+                this.trail_points.splice(1, i);
+        }
+    }
+
+    this.update = function() {
+        this.movement();
+        this.boundaries();
+        this.trail();
+        this.draw();
     }
 }
 
-// SPAWN PLAYER ANT
-// var playerVariable = new PlayerAnt(canvas.width / 2, canvas.height / 2 - home_radius);
-
 // SPAWN ANTS
 var ants = [];
-for (var i = 0; i < 180; i++){
+for (var i = 0; i < 50; i++){
     if ((i * 2) < 90)
         ants.push(new Ant(canvas.width / 2 + (Math.cos((90 - (i * 2)) * (Math.PI / 180)) * home_radius), canvas.height / 2 - (Math.sin((90 - (i * 2)) * (Math.PI / 180)) * home_radius), i * 2));
     else if ((i * 2) < 180 && (i * 2) > 90)
@@ -203,18 +166,8 @@ for (var i = 0; i < 180; i++){
         ants.push(new Ant(canvas.width / 2 - (Math.cos(((i * 2) - 270) * (Math.PI / 180)) * home_radius), canvas.height / 2 - (Math.sin(((i * 2) - 270) * (Math.PI / 180)) * home_radius), i * 2));
 }
 
-// PLAYER CONTROLS
+// CONTROLS
 function aim(e){
-    /*
-    if(e.keyCode == 37 || e.keyCode == 65){
-        left = true;
-    }
-
-    if(e.keyCode == 39 || e.keyCode == 68){
-        right = true;
-    }
-    */
-
     if(e.keyCode == 32){
         if (pause == false){
             pause = true;
@@ -223,64 +176,31 @@ function aim(e){
             pause = false;
         }
     }
-    /*
-
-    if(e.keyCode == 40 || e.keyCode == 83){
-        down = true;
-    }
-    */
 }
-/*
-function aim2(e){
-    if((e.keyCode == 37 && left == true) || (e.keyCode == 65 && left == true)){
-        playerVariable.dx = 0;
-        playerVariable.dy = 0;
-        left = false;
-    }
-
-    if((e.keyCode == 39 && right == true) || (e.keyCode == 68 && right == true)){
-        playerVariable.dx = 0;
-        playerVariable.dy = 0;
-        right = false;
-    }
-
-    if((e.keyCode == 38 && up == true) || (e.keyCode == 87 && up == true) || (e.keyCode == 32 && up == true)){
-        playerVariable.dx = 0;
-        playerVariable.dy = 0;
-        up = false;
-    }
-
-    if((e.keyCode == 40 && down == true) || (e.keyCode == 83 && down == true)){
-        playerVariable.dx = 0;
-        playerVariable.dy = 0;
-        down = false
-    }
-}
-*/
 
 // UPDATE
 function animate(timeStamp){
     if (pause == false){
+        // TIME FPS MANAGEMENT
         secondsPassed = (timeStamp - oldTimeStamp) / 1000;
         oldTimeStamp = timeStamp;
 
+        // CLEAR SCREEN FOR NEXT FRAME
         c.clearRect(0, 0, canvas.width, canvas.height);
 
+        // DRAW ANTS
         for (var i = 0; i < ants.length; i++){
-            ants[i].draw();
+            ants[i].update();
         }
 
-        // GameObjects
-        //playerVariable.draw();
-
-        // HOME
+        // DRAW HOME
         c.beginPath();
         c.arc(canvas.width/2 - (home_radius / 4), canvas.height/2 - (home_radius / 4), home_radius, 0, 2 * Math.PI);
         c.fillStyle = "darkgrey";
         c.fill();
         c.stroke();
 
-        // HELP
+        // DRAW HELP
         c.beginPath();
         c.font = "20px Monospace";
         c.fillStyle = "darkgrey"
@@ -288,11 +208,11 @@ function animate(timeStamp){
         c.fillText('  +     Increment Speed', 10, 40);
         c.fillText('  -     Decrement Speed', 10, 60);
 
+        // RESTORE CANVAS STATE 
         c.restore();
     }
 
     requestAnimationFrame(animate);
-
 }
 
 animate();
